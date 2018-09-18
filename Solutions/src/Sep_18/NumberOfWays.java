b2ec9a51b67ae9f030837b70a14419e7c5eaa274
Saurabh Mithe <Saurabh@saurabhs-MacBook-Pro.local>
2018-09-18 11:59:48 -0700

    added problem description

--- Solutions/src/Sep_18/NumberOfWays.java
+++ Solutions/src/Sep_18/NumberOfWays.java
@@ -2,6 +2,20 @@ package Sep_18;
 
 public class NumberOfWays {
 
+    /*
+
+    There is an N by M matrix of zeroes. Given N and M, write a function to count the number of ways of starting
+    at the top-left corner and getting to the bottom-right corner. You can only move right or down.
+
+    For example, given a 2 by 2 matrix, you should return 2,
+    since there are two ways to get to the bottom-right:
+
+    Right, then down
+    Down, then right
+    Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
+
+    */
+
     public static void main(String[] args) {
         int[][] maze = new int[9][9];
         NumberOfWays numberOfWays = new NumberOfWays();
